1c41a9149341dc81a594c73acf9fd7a258496bfd
Ricky Lin <rlin61@stuy.edu>
2025-10-28 10:39:14 -0400

    Added basic bones of functional app

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from flask import render_template
 
+app = Flask("__main__")
+
+@app.route("/")
+def homepage():
+    return "this is homepage"
+
+app.debug = True
+app.run()
