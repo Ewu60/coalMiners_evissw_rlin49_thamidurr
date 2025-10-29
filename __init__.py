e3da24f1b49ab184152aa08e2a202c3d67ee230b
Tobber55 <thamidur.haruki@gmail.com>
2025-10-29 10:46:37 -0400

    stuff

--- __init__.py
+++ __init__.py
@@ -3,8 +3,6 @@ from flask import render_template
 import sqlite3
 import csv
 import os
-from flask import request           #facilitate form submission
-from flask import session           #facilitate cookies
 
 
 app = Flask("__main__")
@@ -15,9 +13,9 @@ os.remove(DB_FILE)
 db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
-usernames = []
-passwords = []
-bios = []
+usernames = ["ricky", "ewu"]
+passwords = ["rickyp", "ewup"]
+bios = ["rickys", "ewus"]
 
 links = []
 names = []
@@ -30,11 +28,16 @@ c.execute("CREATE TABLE account (user TEXT, password TEXT, bios TEXT)")
 c.execute("CREATE TABLE page (link TEXT, name TEXT, content TEXT)")
 
 for i in range(len(usernames)):
-    c.execute("INSERT INTO account VALUES (?, ?, ?)", (username[i], passwords[i], bios[i]))
+    c.execute("INSERT INTO account VALUES (?, ?, ?)", (usernames[i], passwords[i], bios[i]))
 for i in range(len(links)):
     c.execute("INSERT INTO page VALUES (?, ?, ?)", (links[i], names[i], content[i]))
 
 #==========================================================
+
+print("\n========== LIST OF ALL STUDENTS ==========")
+for row in c.execute("SELECT user FROM account"):
+    print(row[0])
+print("========== END OF STUDENT LIST ==========\n")
 
 @app.route("/")
 def homepage():
